1faac121410245aa47ecaca20af6011108aaece2
andersongs-dev <andersongsw@gamil.com>
2021-01-15 07:43:46 -0300

    Adicionar texto exemplo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,7 +47,7 @@
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
-                            Teste
+                            Inserir algum texto
                         </div>
                     </div>
                 </div>
